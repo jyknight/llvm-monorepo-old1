9c62118aa8e7463c73c9d458b8c2ab138f37f525
geoffray <geoffray@localhost>
2010-08-30 20:08:26 +0000

    Create a precise branch to get precise information on ip instead of doing an approximate search.
    llvm-svn=112508

--- clang/test/CXX/dcl.dcl/basic.namespace/namespace.def/p1.cpp
+++
@@ -1,7 +0,0 @@
-// RUN: %clang_cc1 -fsyntax-only -verify -std=c++0x %s
-
-namespace a {} // original
-namespace a {} // ext
-inline namespace b {} // inline original
-inline namespace b {} // inline ext
-inline namespace {} // inline unnamed
